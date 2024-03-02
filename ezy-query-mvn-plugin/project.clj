a78882459d07831fc26a2d6b0c5a2df4d933bba1
Ronald Kayondo <noryak1234@gmail.com>
2024-03-02 22:30:44 +0000

    un-deployed should have snapshot

--- ezy-query-mvn-plugin/project.clj
+++ ezy-query-mvn-plugin/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.kayr/ezy-query-maven-plugin "0.0.1"
+(defproject io.github.kayr/ezy-query-maven-plugin "0.0.1-SNAPSHOT"
   :description "A maven plugin for generating ezy-query code"
   :url "https://github.com/kayr/ezy-query-maven"
   :license {:name "Apache License, Version 2.0"
